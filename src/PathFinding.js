a48b27ce9ca133368b3f42adc63f221058e04a6a
Evan You <yyou@google.com>
2014-06-11 17:29:13 -0400

    only use AStar

--- src/PathFinding.js
+++ src/PathFinding.js
@@ -4,16 +4,5 @@ module.exports = {
     'Grid'                      : require('./core/Grid'),
     'Util'                      : require('./core/Util'),
     'Heuristic'                 : require('./core/Heuristic'),
-    'AStarFinder'               : require('./finders/AStarFinder'),
-    'BestFirstFinder'           : require('./finders/BestFirstFinder'),
-    'BreadthFirstFinder'        : require('./finders/BreadthFirstFinder'),
-    'DijkstraFinder'            : require('./finders/DijkstraFinder'),
-    'BiAStarFinder'             : require('./finders/BiAStarFinder'),
-    'BiBestFirstFinder'         : require('./finders/BiBestFirstFinder'),
-    'BiBreadthFirstFinder'      : require('./finders/BiBreadthFirstFinder'),
-    'BiDijkstraFinder'          : require('./finders/BiDijkstraFinder'),
-    'IDAStarFinder'             : require('./finders/IDAStarFinder'),
-    'JumpPointFinder'           : require('./finders/JumpPointFinder'),
-    'OrthogonalJumpPointFinder' : require('./finders/OrthogonalJumpPointFinder'),
-    'TraceFinder'               : require('./finders/TraceFinder')
+    'AStarFinder'               : require('./finders/AStarFinder')
 };
